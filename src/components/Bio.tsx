963b0f976d15b2aab088686907b8370690e73a28
Cristobal Salinas <contacto.csalinas@gmail.com>
2023-11-18 01:08:04 -0300

    :art: agregando biografia

---
+++ src/components/Bio.tsx
@@ -0,0 +1,41 @@
+import BioDescription from "./BioDescription";
+
+export default function Bio() {
+  const bioList = [
+    {
+      title: "1996",
+      description: "Nacido en Viña del mar",
+    },
+    {
+      title: "2018",
+      description: "Titulado de ingeniería en informática Duoc Uc.",
+    },
+    {
+      title: "2019",
+      description: "Evualueserve, Full-Stack developer",
+    },
+    {
+      title: "2021",
+      description: "Jumpitt Labs, Full-Stack developer",
+    },
+    {
+      title: "2023",
+      description: "Trabajando como Freelancer e Indie Developer",
+    },
+  ];
+
+  return (
+    <>
+      <h2 className="mb-3 mt-5 text-lg">
+        <u>Bio</u>
+      </h2>
+      <div className="w-full">
+        {bioList.map(bio => (
+          <>
+            <BioDescription title={bio.title} description={bio.description} />
+          </>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ src/components/BioDescription.tsx
@@ -0,0 +1,14 @@
+export interface Props {
+  title: string;
+  description: string;
+}
+
+const BioDescription = ({ title, description }: Props) => {
+  return (
+    <p>
+      <b>{title}:</b> {description}
+    </p>
+  );
+};
+
+export default BioDescription;
