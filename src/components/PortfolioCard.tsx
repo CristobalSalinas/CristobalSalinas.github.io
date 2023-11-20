bb324ffc4d7ebd0cee122b025745c4cdc192a247
Cristobal Salinas <contacto.csalinas@gmail.com>
2023-11-20 18:04:02 -0300

    :art: agregando imagenes a las vistas de mi portafolio

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -9,7 +9,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, description } = frontmatter;
+  const { title, pubDatetime, description, image } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
@@ -18,6 +18,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
 
   return (
     <li className="my-6">
+      <img src={image} height={500} width={500} />
       <a
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
