57554c0b9911839f3bc627e667a1c5317b1567f8
CristobalSalinas <contacto.csalinas@gmail.com>
2023-11-20 22:37:09 +0100

    :construction: construyendo imagenes dinamicas para portafolio

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -18,7 +18,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
 
   return (
     <li className="my-6">
-      <img src={image} height={500} width={500} />
+      <img src={`/assets/${image}`} height={500} width={500} />
       <a
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
--- src/content/portfolio/portafolio-1.md
+++ src/content/portfolio/portafolio-1.md
@@ -3,7 +3,7 @@ title: Portafolio 1
 description: Descripcion de mi blog de prueba
 pubDatetime: 2022-09-23T15:22:00Z
 tags: [prueba]
-image: ../../../public/assets/portfolio1.jpg
+image: portfolio1.jpg
 ---
 
 # mi primer proyecto
